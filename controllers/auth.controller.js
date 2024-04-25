b3435aa5bd4cddd019847378f4471bc417671bff
hamza-choudhary <humzach700@gmail.com>
2024-04-25 11:24:22 +0500

    fix: refresh token removed on logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,57 +12,60 @@ import { asyncHandler } from '../helpers/asyncHandler.js'
  * @param {import('express').NextFunction} next - The next middleware function.
  */
 export const postSignup = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body
-  const role = req?.user?.role ? USER_ROLES.CASHIER : USER_ROLES.CUSTOMER
+	const { email, password } = req.body
+	const role = req?.user?.role ? USER_ROLES.CASHIER : USER_ROLES.CUSTOMER
 
-  const hashedPassword = await bcrypt.hash(password, 10)
+	const hashedPassword = await bcrypt.hash(password, 10)
 
-  const result = await prisma.user.create({
-    data: { email, password: hashedPassword, role },
-    select: { id: true, email: true, role: true },
-  })
+	const result = await prisma.user.create({
+		data: { email, password: hashedPassword, role },
+		select: { id: true, email: true, role: true },
+	})
 
-  const accessToken = jwt.sign(result, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  })
-  const refreshToken = jwt.sign(result, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-  })
+	const accessToken = jwt.sign(result, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+	})
+	const refreshToken = jwt.sign(result, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+	})
 
-  await prisma.user.update({
-    where: { id: result.id },
-    data: { refreshToken },
-  })
+	await prisma.user.update({
+		where: { id: result.id },
+		data: { refreshToken },
+	})
 
-  res.cookie(ACCESS_TOKEN, accessToken, { httpOnly: true, secure: true })
-  res.status(201).json({
-    refreshToken,
-    data: result,
-    message: MESSAGES.SUCCESS.AUTH.SIGNUP,
-  })
+	res.cookie(ACCESS_TOKEN, accessToken, { httpOnly: true, secure: true })
+	res.status(201).json({
+		refreshToken,
+		data: result,
+		message: MESSAGES.SUCCESS.AUTH.SIGNUP,
+	})
 })
 
-export const postLogout = (req, res, next) => {
-  // FIXME: delete refresh token from db as well 
-  // and we have to make logout route authenticated then we will get users user.id
-  res.clearCookie(ACCESS_TOKEN)
-  res.status(STATUS.OK).json({ message: MESSAGES.SUCCESS.AUTH.LOGOUT })
-}
+export const postLogout = asyncHandler(async (req, res, next) => {
+	await prisma.user.update({
+		where: { id: req.user.id },
+		data: { refreshToken: '' },
+	})
+
+	res.clearCookie(ACCESS_TOKEN)
+	res.status(STATUS.OK).json({ message: MESSAGES.SUCCESS.AUTH.LOGOUT })
+})
 
 export const postRefreshToken = asyncHandler(async (req, res, next) => {
-  const user = req.user
+	const user = req.user
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  })
+	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+	})
 
-  res.cookie(ACCESS_TOKEN, accessToken, {
-    httpOnly: true,
-    secure: true,
-    overwrite: true,
-  })
+	res.cookie(ACCESS_TOKEN, accessToken, {
+		httpOnly: true,
+		secure: true,
+		overwrite: true,
+	})
 
-  res
-    .status(STATUS.OK)
-    .json({ message: MESSAGES.SUCCESS.ACCESS_TOKEN_REFRESHED })
+	res
+		.status(STATUS.OK)
+		.json({ message: MESSAGES.SUCCESS.ACCESS_TOKEN_REFRESHED })
 })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 import { USER_ROLES } from '../constants/global-constant.js'
 import { ROUTES } from '../constants/routes.js'
 import {
-  postLogout,
-  postRefreshToken,
-  postSignup,
+	postLogout,
+	postRefreshToken,
+	postSignup,
 } from '../controllers/auth.controller.js'
 import {
-  authentication,
-  authorization,
-  verifyRefresh,
+	authentication,
+	authorization,
+	verifyRefresh,
 } from '../middleware/auth.middleware.js'
 import { handleValidationErrors } from '../middleware/error.middleware.js'
 import { validateSignup } from '../middleware/validators/validateSignup.js'
@@ -17,20 +17,20 @@ import { validateSignup } from '../middleware/validators/validateSignup.js'
 const router = Router()
 
 router
-  .route(ROUTES.AUTH.CUSTOMER_SIGNUP)
-  .post(validateSignup(), handleValidationErrors, postSignup)
+	.route(ROUTES.AUTH.CUSTOMER_SIGNUP)
+	.post(validateSignup(), handleValidationErrors, postSignup)
 
 router
-  .route(ROUTES.AUTH.CASHIER_SIGNUP)
-  .post(
-    authentication,
-    authorization(USER_ROLES.ADMIN),
-    validateSignup(),
-    handleValidationErrors,
-    postSignup,
-  )
+	.route(ROUTES.AUTH.CASHIER_SIGNUP)
+	.post(
+		authentication,
+		authorization(USER_ROLES.ADMIN),
+		validateSignup(),
+		handleValidationErrors,
+		postSignup
+	)
 
-router.route(ROUTES.AUTH.LOGOUT).post(postLogout)
+router.route(ROUTES.AUTH.LOGOUT).post(authentication, postLogout)
 
 router.route(ROUTES.AUTH.REFRESH_TOKEN).post(verifyRefresh, postRefreshToken)
 
